0782162deeb1d96f4a33318f089fee41e9511202
Chris Mague <github@mague.com>
2019-04-01 09:47:08 -0700

    output to csv

--- audit-elasticache.go
+++ audit-elasticache.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elasticache"
+	"os"
 )
 
 func listRegions() []string {
@@ -22,10 +24,13 @@ func listRegions() []string {
 	return regions
 }
 
-func listCaches(region string, results chan<- []*elasticache.CacheCluster) {
-	sess, _ := session.NewSession(&aws.Config{
+func listCaches(region string, results chan<- []*elasticache.CacheCluster, outf *csv.Writer) {
+	sess, sesserr := session.NewSession(&aws.Config{
 		Region: aws.String(region)},
 	)
+	if sesserr != nil {
+		fmt.Println(os.Stderr, sesserr.Error())
+	}
 	svc := elasticache.New(sess)
 	input := &elasticache.DescribeCacheClustersInput{}
 	result, err := svc.DescribeCacheClusters(input)
@@ -33,28 +38,41 @@ func listCaches(region string, results chan<- []*elasticache.CacheCluster) {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
 			case elasticache.ErrCodeCacheClusterNotFoundFault:
-				fmt.Println(elasticache.ErrCodeCacheClusterNotFoundFault, aerr.Error())
+				//fmt.Println(os.Stderr, "DAMMIT1", elasticache.ErrCodeCacheClusterNotFoundFault, aerr.Error())
 				results <- result.CacheClusters
 			case elasticache.ErrCodeInvalidParameterValueException:
-				fmt.Println(elasticache.ErrCodeInvalidParameterValueException, aerr.Error())
+				//fmt.Println(os.Stderr, "DAMMIT2", elasticache.ErrCodeInvalidParameterValueException, aerr.Error())
 				results <- result.CacheClusters
 			case elasticache.ErrCodeInvalidParameterCombinationException:
-				fmt.Println(elasticache.ErrCodeInvalidParameterCombinationException, aerr.Error())
+				//fmt.Println(os.Stderr, "DAMMIT3", elasticache.ErrCodeInvalidParameterCombinationException, aerr.Error())
 				results <- result.CacheClusters
 			default:
-				fmt.Println(aerr.Error())
+				//fmt.Println(os.Stderr, aerr.Error())
 				results <- result.CacheClusters
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
 			// Message from an error.
-			fmt.Println(err.Error())
+			fmt.Println(os.Stderr, "DAMMIT" < err.Error())
 
 		}
 		return
 	}
 
-	fmt.Println(result.CacheClusters)
+	for _, r := range result.CacheClusters {
+		//fmt.Println(r)
+		if err := outf.Write([]string{
+			*r.CacheClusterId,
+			*r.CacheClusterStatus,
+			region,
+			*r.PreferredAvailabilityZone,
+			*r.CacheNodeType,
+			*r.CacheParameterGroup.CacheParameterGroupName,
+			*r.EngineVersion}); err != nil {
+			fmt.Println("error writing record to csv:", err)
+		}
+	}
+
 	results <- result.CacheClusters
 
 }
@@ -63,13 +81,27 @@ func main() {
 	regions := listRegions()
 	results := make(chan []*elasticache.CacheCluster, len(regions))
 
+	outf := csv.NewWriter(os.Stdout)
+	if err := outf.Write([]string{
+		"name",
+		"status",
+		"region",
+		"availability_zone",
+		"node_type",
+		"parameter_group",
+		"version"}); err != nil {
+		fmt.Println("error writing record to csv:", err)
+	}
+
 	//start up a worker for each region
 	for w := 0; w < len(regions); w++ {
-		go listCaches(regions[w], results)
+		go listCaches(regions[w], results, outf)
 	}
 	for a := 1; a <= len(regions); a++ {
 		<-results
 	}
+
 	close(results)
+	outf.Flush()
 
 }
